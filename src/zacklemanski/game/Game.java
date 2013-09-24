71f415affb71b9f0f0d30d2edd2aa340c366372c
CA-Remix <zackscottl@yahoo.com>
2013-09-24 16:50:07 -0400

    update

--- src/zacklemanski/game/Game.java
+++ src/zacklemanski/game/Game.java
@@ -1,26 +1,45 @@
 package zacklemanski.game;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable {
 	public static int width = 300;
-
+	public static boolean running = false;
 	public static int height = width * 16 / 9;
-	public int scale = 3;
+	public static int scale = 3;
 	private static final long serialVersionUID = 1L;
-
+	public static JFrame frame = new JFrame();
 	private Thread thread;
+	static Dimension size = new Dimension(width * scale, height * scale);
 
 	public static void main(String[] args) {
+		Game game = new Game(size);
+		frame.setPreferredSize(size);
+		frame.setResizable(false);
+		frame.setTitle("Game");
+		frame.add(game);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+	}
+
+	public Game(Dimension size) {
 
 	}
 
 	public synchronized void start() {
-		thread = new Thread(this, "Display/Game or whatever u want");
-		thread.start();
+		while (running) {
+			thread = new Thread(this, "Display/Game or whatever u want");
+			thread.start();
+		}
 	}
 
 	public synchronized void stop() {
+		running = false;
 		try {
 			thread.join();
 		} catch (InterruptedException e) {
