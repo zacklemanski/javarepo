611c77f457e486a635948f8a4c6e9dbef7a296b6
CA-Remix <zackscottl@yahoo.com>
2013-09-24 16:19:37 -0400

    update

--- src/zacklemanski/game/Game.java
+++ src/zacklemanski/game/Game.java
@@ -3,11 +3,13 @@ package zacklemanski.game;
 import java.awt.Canvas;
 
 public class Game extends Canvas implements Runnable {
-	public int width = 800;
+	public static int width = 300;
 
-	public int height = width * 16 / 9;
+	public static int height = width * 16 / 9;
 	public int scale = 3;
 	private static final long serialVersionUID = 1L;
+
+	private Thread thread;
 
 	public static void main(String[] args) {
 
