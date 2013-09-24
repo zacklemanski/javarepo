700c5ab2c3a4a33fd0845b96b4864cb169d611d1
Joao Lourenco <joaoguerralourenco@hotmail.com>
2013-09-24 20:02:40 +0100

    fixed

--- src/zacklemanski/game/Game.java
+++ src/zacklemanski/game/Game.java
@@ -2,23 +2,15 @@ package zacklemanski.game;
 
 import java.awt.Canvas;
 
-
 public class Game extends Canvas implements Runnable {
-
-	/**
-	 * 
-	 */
-
 	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) {
-		
+
 	}
-	@Override
-	public void run() {	
-		public void gameLoop() {
-			
-		}
+
+	public void run() {
+
 	}
-	
+
 }
